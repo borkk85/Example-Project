056b4387f4a94944ac56416c62865e191ed91aae
Borko <bor.kos.mk@gmail.com>
2022-05-12 10:21:25 +0200

    Add files via upload

--- func.js
+++ func.js
@@ -3,9 +3,6 @@ const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
-// Scroll to top selection
-const scrollUp = document.querySelector("#scroll-up");
-
 // Select nav links
 const navLink = document.querySelectorAll(".nav-link");
 
@@ -21,11 +18,3 @@ navLink.forEach((link) =>
   })
 );
 
-// scroll to top functionality
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
